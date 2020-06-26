61b39175c5c149671b9bff4e5ab0289ad4ddb9d9
Tony Park <tonypark0403@gmail.com>
2020-06-25 23:31:00 -0400

    validated password

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,11 +5,19 @@ import { AuthService } from './auth.service';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
   @Post('/signup')
   async signUp(
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
   ): Promise<void> {
     // console.log(authCredentialsDto);
     return this.authService.signUp(authCredentialsDto);
+  }
+
+  @Post('/signin')
+  async signIn(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<string> {
+    return this.authService.signIn(authCredentialsDto);
   }
 }
